1054679c6a4c35c39d34eecc19ab18ce71fffd46
riddlej9879 <riddlej9879@yahoo.com>
2023-01-26 14:11:51 -0600

    Setting up start button

--- app/src/main/java/com/example/lab1arockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/lab1arockpaperscissors/MainActivity.java
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityManager;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityManager binding;
+
+    private RockPaperScissor rps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RockPaperScissor rps = new RockPaperScissor();
+        rps = new RockPaperScissor(false);
     }
 
+
+    public void ChangeState(View view) {
+
+        if (rps.isRunning()) {
+            rps.setRunning(false);
+            // Change button to start
+        } else {
+            rps.setRunning(true);
+            // Change button to stop
+        }
+    }
 }
--- app/src/main/java/com/example/lab1arockpaperscissors/RockPaperScissor.java
+++ app/src/main/java/com/example/lab1arockpaperscissors/RockPaperScissor.java
@@ -1,4 +1,19 @@
 package com.example.lab1arockpaperscissors;
 
+import android.widget.TextView;
+
 public class RockPaperScissor {
+    private boolean running;
+
+    public RockPaperScissor(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
 }
